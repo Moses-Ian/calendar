8de7fe2a76f407444f32ff2cba6eb55ad17acf07
Expired <imoses2@hotmail.com>
2022-01-13 15:46:24 -0700

    restructured html

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,13 @@ function update() {
 	currentDay.text(moment().format("dddd, MMMM Do"));
 	taskContent = $(".time-block");
 	taskContent.each(function() {
-		var task = $(this).children(".task-content");
-		var timeContent = $(this).children(".time-content");
+		var task = $(this).find(".task-container");
+		var timeContent = $(this).find(".time-content");
 		task.removeClass("bg-primary bg-secondary bg-success");
 		
-		//just to test
-		// if (time.text() === "3pm"){
-			// task.addClass("bg-primary");
-		// } else {
-			// task.addClass("bg-secondary");
-		// }
-		
 		var time = moment(timeContent.text().trim(), "h a");
-		// console.log(a.format("dddd, MMMM Do YYYY @ hh:mm a"));
 		
 		var minutesSinceTimeBlock = moment().diff(time, "minutes");
-		console.log(minutesSinceTimeBlock);
 		
 		if (minutesSinceTimeBlock >= 60) {
 			task.addClass("bg-secondary");
@@ -60,12 +51,16 @@ function update() {
 //================================================
 
 //build the time blocks
-for(let i=10; i<=17; i++) {
+for(let i=9; i<=17; i++) {
 	var rowEl = $("<div>").addClass("time-block row border border-primary rounded");
-	var timeEl = $("<div>").addClass("time-content col-2 border-right border-dark").text(moment().set("hour", i).format("h a"));	//I don't think this is quite how I want to do this
+	var timeEl = $("<div>").addClass("time-container col-2 border-right border-dark");	//I don't think this is quite how I want to do this
 		//-> I want to save the time as a variable I can compare to later
-	var taskEl = $("<div>").addClass("task-content col-10").text("default");
+	var pEl = $("<p>").addClass("time-content").text(moment().set("hour", i).format("h a"));
+	var taskEl = $("<div>").addClass("task-container col-10");
+	var ppEl = $("<p>").addClass("task-content").text("default");
 	
+	timeEl.append(pEl);
+	taskEl.append(ppEl);
 	rowEl.append(timeEl, taskEl);
 	timeContainer.append(rowEl);
 	
