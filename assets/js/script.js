e2f61ce1adb617144121ec8e16746a3c727f9340
Expired <imoses2@hotmail.com>
2022-01-13 15:36:31 -0700

    color code based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,45 @@
 //================================================
 var currentDay = $("#currentDay");
 var timeContainer = $("#time-container");
-
+var taskContent;
 
 
 
 //function declarations
 //================================================
-
+function update() {
+	//display current day
+	currentDay.text(moment().format("dddd, MMMM Do"));
+	taskContent = $(".time-block");
+	taskContent.each(function() {
+		var task = $(this).children(".task-content");
+		var timeContent = $(this).children(".time-content");
+		task.removeClass("bg-primary bg-secondary bg-success");
+		
+		//just to test
+		// if (time.text() === "3pm"){
+			// task.addClass("bg-primary");
+		// } else {
+			// task.addClass("bg-secondary");
+		// }
+		
+		var time = moment(timeContent.text().trim(), "h a");
+		// console.log(a.format("dddd, MMMM Do YYYY @ hh:mm a"));
+		
+		var minutesSinceTimeBlock = moment().diff(time, "minutes");
+		console.log(minutesSinceTimeBlock);
+		
+		if (minutesSinceTimeBlock >= 60) {
+			task.addClass("bg-secondary");
+		} else if(minutesSinceTimeBlock >= 0) {
+			task.addClass("bg-primary");
+		} else {
+			task.addClass("bg-success");
+		}
+		
+		
+	});
+}
 
 
 
@@ -26,23 +58,20 @@ var timeContainer = $("#time-container");
 
 //main code
 //================================================
-//display current day
-//this will probably trigger on update
-currentDay.text(moment().format("dddd, MMMM Do"));
 
 //build the time blocks
 for(let i=10; i<=17; i++) {
-	var rowEl = $("<div>").addClass("row");
-	var timeEl = $("<div>").addClass("col-2").text(moment().set("hour", i).format("h a"));	//I don't think this is quite how I want to do this
+	var rowEl = $("<div>").addClass("time-block row border border-primary rounded");
+	var timeEl = $("<div>").addClass("time-content col-2 border-right border-dark").text(moment().set("hour", i).format("h a"));	//I don't think this is quite how I want to do this
 		//-> I want to save the time as a variable I can compare to later
-	var taskEl = $("<div>").addClass("col-10").text("default");
+	var taskEl = $("<div>").addClass("task-content col-10").text("default");
 	
 	rowEl.append(timeEl, taskEl);
 	timeContainer.append(rowEl);
 	
 }
 
-
+update();
 
 
 
